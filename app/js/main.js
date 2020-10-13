5b5e36e0791e6ed1cfb3314913280f569a1df88f
IAndresI <andrei200277@gmail.com>
2020-10-13 14:34:26 +0300

    index without adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,49 +1,38 @@
 "use strict";
 import tabs from './tabs';
+import selectStyler from './selectStyler';
+import slider from './slider';
+import runningNumbers from './runningNumbers';
 
 document.addEventListener("DOMContentLoaded", function () {
 
-  function selectStyler({
-    select,
-    placeholder = ""
-  }) {
-    let selectContainer = document.createElement("div"),
-      inputContainer = document.createElement("div"),
-      inputArray = document.querySelectorAll(`#${select.getAttribute("id").replace(/ /g, ".")} option`),
-      button = document.createElement("button");
-    select.style.display = "none";
-    selectContainer.append(button);
-    selectContainer.append(inputContainer);
-    selectContainer.classList.add("select");
-    button.classList.add("select__button");
-    button.type = "button";
-    button.textContent = placeholder;
-    inputContainer.classList.add("select__option-container");
-    inputContainer.style.transition = "all .5s";
-    inputArray.forEach((e) => {
-      let input = document.createElement("input"),
-        label = document.createElement("label");
-      input.classList.add("select__option");
-      input.type = "radio";
-      input.name = "1";
-      label.classList.add("select__label");
-      label.textContent = e.innerHTML;
-      input.addEventListener("click", () => {
-        document.querySelectorAll(".select__label").forEach(e => {
-          e.classList.remove("select__label--active");
-        });
-        label.classList.add("select__label--active");
-        inputContainer.classList.remove("select__option-container--active");
-        button.textContent = label.textContent;
-      });
-      label.append(input);
-      inputContainer.append(label);
-    });
-    button.addEventListener("click", () => {
-      inputContainer.classList.toggle("select__option-container--active");
-    });
-    select.insertAdjacentElement("afterend", selectContainer);
-  }
+  runningNumbers({
+    parent: document.querySelectorAll(".statistic__counter")[0],
+    prefix: "K",
+    speed: 3,
+    addElementsToScroll: 10,
+    gradually: true
+  });
+  runningNumbers({
+    parent: document.querySelectorAll(".statistic__counter")[1],
+    prefix: "K",
+    speed: 3,
+    addElementsToScroll: 10,
+    gradually: true
+  });
+  runningNumbers({
+    parent: document.querySelectorAll(".statistic__counter")[2],
+    speed: 3,
+    addElementsToScroll: 10,
+    gradually: true
+  });
+  runningNumbers({
+    parent: document.querySelectorAll(".statistic__counter")[3],
+    prefix: "K",
+    speed: 3,
+    addElementsToScroll: 10,
+    gradually: true
+  });
 
   selectStyler({
     select: document.querySelector(".appointment__input"),
@@ -62,4 +51,14 @@ document.addEventListener("DOMContentLoaded", function () {
     tabToShow: 0
   });
 
+  slider({
+    slider__item: ".review__item",
+    slidesToShow: 2,
+    speed: 1000,
+    dots: false,
+    responsive: [{
+      breakPoint: 780,
+      slidesToShow: 1
+    }]
+  });
 });
---
+++ app/js/slider.js
@@ -0,0 +1,228 @@
+function slider({
+  slider__item,
+  slider__button__next,
+  slider__button__prev,
+  slidesToShow = 1,
+  responsive = false,
+  slidesToScroll = 1,
+  dots = true,
+  speed = 700,
+  fade = false
+}) {
+
+  let slider_item = document.querySelectorAll(slider__item),
+    slider_translatex = document.createElement("div"),
+    slider_button_next = document.querySelector(slider__button__next),
+    slider_button_prev = document.querySelector(slider__button__prev),
+    slidesToShowOriginal = slidesToShow,
+    slidesToScrollOriginal = slidesToScroll,
+    temp = 0;
+
+  // Create slides-wrapper
+
+  if (slider_item[0]) {
+    slider_translatex.classList.add("slider__translatex");
+    slider_translatex.style.display = "flex";
+    if (!fade) slider_translatex.style.transition = `transform ${speed/1000}s`;
+    let documentWidth = document.documentElement.clientWidth,
+      parent = slider_item[0].parentElement,
+      innerSliderWidth = slider_item[0].parentElement.offsetWidth,
+      slide_width = innerSliderWidth / slidesToShow,
+      slider_container_array = [];
+
+    for (let i = 0; i < slider_item.length; i++) {
+      let slide_container = document.createElement("div");
+      slide_container.classList.add("slide-container");
+      slide_container.style.width = slide_width + "px";
+      let slide_container_content = slider_item[i].cloneNode(true);
+      slide_container.append(slide_container_content);
+      slider_container_array.push(slide_container);
+    }
+    parent.innerHTML = "";
+    slider_container_array.forEach(element => {
+      slider_translatex.append(element);
+    });
+
+    slider_translatex.style.width = slider_item.length * slide_width + "px";
+    parent.append(slider_translatex);
+
+    // Responsive
+
+    function adaptive(slidesScroll, slidesShow) {
+      slidesToScrollOriginal = slidesScroll || slidesToScroll;
+      let slides = document.querySelectorAll(`.${slider_translatex.parentElement.getAttribute("class").replace(/ /g,".")} .slide-container`),
+        innerSliderWidth = slider_translatex.parentElement.offsetWidth;
+      slides.forEach(element => {
+        element.style.width = (innerSliderWidth / slidesShow) + "px";
+      });
+      slider_translatex.style.width = slider_item.length * parseInt(slides[0].style.width) + "px";
+      slidesToShowOriginal = slidesShow;
+      slidesToScrollOriginal = slidesScroll;
+      setDots();
+    }
+
+    if (dots) {
+      createDots();
+      dotClick(0);
+    }
+
+    if (responsive) {
+      setDots();
+      let biggestBreakPoint = 0;
+      slider_translatex.style.transform = `translateX(-${ (((parseInt(slider_translatex.style.width) / slider_item.length) * temp)) * slidesToScrollOriginal}px)`;
+
+      responsive.forEach(element => {
+        if (documentWidth < element.breakPoint) {
+          adaptive(element.slidesToScroll, element.slidesToShow);
+        }
+      });
+
+      window.addEventListener(`resize`, () => {
+        documentWidth = document.documentElement.clientWidth;
+        responsive.forEach(element => {
+          if (documentWidth < element.breakPoint) {
+            adaptive(element.slidesToScroll, element.slidesToShow);
+          }
+          if (element.breakPoint > biggestBreakPoint) biggestBreakPoint = element.breakPoint;
+        });
+        if (documentWidth > biggestBreakPoint) {
+          adaptive(slidesToScroll, slidesToShow);
+        }
+        slider_translatex.style.transform = `translateX(-${ (((parseInt(slider_translatex.style.width) / slider_item.length) * temp)) * slidesToScrollOriginal}px)`;
+      });
+    } else {
+      setDots();
+      window.addEventListener(`resize`, () => {
+        adaptive(slidesToScroll, slidesToShow);
+        slider_translatex.style.transform = `translateX(-${ (((parseInt(slider_translatex.style.width) / slider_item.length) * temp)) * slidesToScrollOriginal}px)`;
+      });
+    }
+
+    // Dots
+
+    function createDots() {
+      let dots_number = Math.ceil((slider_item.length - slidesToShowOriginal) / slidesToScrollOriginal) + 1,
+        dots_container = document.createElement("ul");
+      dots_container.classList.add("slider__dots-list");
+      parent.append(dots_container);
+      for (let i = 0; i < dots_number; i++) {
+        let dot = document.createElement("li"),
+          dot_button = document.createElement("button");
+        dot.classList.add("slider__dot");
+        dot_button.classList.add("slider__dot-button");
+        dot_button.innerHTML = `${i}`;
+        dot.append(dot_button);
+        dots_container.append(dot);
+        dot.addEventListener("click", function () {
+          dotClick(i);
+          scrollSlide(i);
+        });
+      }
+    }
+
+    function clearDots() {
+      document.querySelector(".slider__dots-list").remove();
+    }
+
+    function dotClick(target) {
+      dots = document.querySelectorAll(`.${slider_translatex.parentElement.getAttribute("class").replace(/ /g,".")} .slider__dot-button`);
+      for (let j = 0; j < dots.length; j++) {
+        if (dots[j] == dots[target]) dots[j].classList.add("slider__dot-button--active");
+        else dots[j].classList.remove("slider__dot-button--active");
+      }
+      temp = target;
+    }
+
+    function setDots() {
+      if (dots) {
+        clearDots();
+        createDots();
+        dotClick(temp);
+      }
+    }
+
+    // Slide Scroll
+
+    function scrollSlide(numberOfSlide) {
+      let maxNextButtonClickCounter = Math.ceil((slider_item.length - slidesToShowOriginal) / slidesToScrollOriginal);
+      if (slider_item.length <= slidesToScrollOriginal) slidesToScrollOriginal = 0;
+      if (numberOfSlide > maxNextButtonClickCounter) numberOfSlide = temp = 0;
+      else if (numberOfSlide < 0) numberOfSlide = temp = maxNextButtonClickCounter;
+      if (dots) dotClick(numberOfSlide);
+      if (fade == true) {
+        let animateID,
+          slider_item = document.querySelectorAll(slider__item),
+          durationProcent = speed / 100,
+          lastTime = performance.now();
+        slider_item.forEach(element => {
+          element.style.transition = `all ${speed/1000}s ease`;
+        });
+
+        function animation() {
+          let currentTime = performance.now();
+          if (currentTime - lastTime >= 0) {
+            slider_item.forEach(element => {
+              element.style.opacity = 0;
+            });
+          }
+          if (currentTime - lastTime >= durationProcent * 100) {
+            slider_item.forEach(element => {
+              slider_translatex.style.transform = `translateX(-${ (((parseInt(slider_translatex.style.width) / slider_item.length) * numberOfSlide)) * slidesToScrollOriginal}px)`;
+              element.style.opacity = 1;
+            });
+            animateID = cancelAnimationFrame(animation);
+          }
+          if (currentTime - lastTime < durationProcent * 100) animateID = requestAnimationFrame(animation);
+        }
+        animateID = requestAnimationFrame(animation);
+      } else {
+        slider_translatex.style.transform = `translateX(-${ (((parseInt(slider_translatex.style.width) / slider_item.length) * numberOfSlide)) * slidesToScrollOriginal}px)`;
+      }
+    }
+
+    if (slider__button__next) {
+      slider_button_next.addEventListener("click", () => {
+        scrollSlide(++temp);
+      });
+    }
+    if (slider__button__prev) {
+      slider_button_prev.addEventListener("click", () => {
+        scrollSlide(--temp);
+      });
+    }
+
+    // Swipes
+
+    let startX = 0,
+      endX = 0;
+
+    function scrollSlideOnTouch() {
+      if (endX > startX + 70) {
+        scrollSlide(--temp);
+      }
+      if (endX < startX - 70) {
+        scrollSlide(++temp);
+      }
+    }
+
+    slider_translatex.addEventListener("mousedown", (event) => {
+      startX = event.screenX;
+    });
+
+    slider_translatex.addEventListener("mouseup", (event) => {
+      endX = event.screenX;
+      scrollSlideOnTouch();
+    });
+
+    slider_translatex.addEventListener("touchstart", (event) => {
+      startX = event.touches[0].clientX;
+    });
+
+    slider_translatex.addEventListener("touchend", (event) => {
+      endX = event.changedTouches[0].clientX;
+      scrollSlideOnTouch();
+    });
+  }
+}
+
+export default slider;
